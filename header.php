5d6ee55d8695d5d608b35ac5a67186f962005baa
Patrick Cameron <patcameron@gmail.com>
2016-01-23 15:35:31 -0500

    Moved Google Analytics code to header

--- header.php
+++ header.php
@@ -34,6 +34,17 @@ ARROW ANIMATION: Justin Aguilar http://www.justinaguilar.com/animations/ -->
   <?php // Load our CSS ?>
   <link rel="stylesheet" type="text/css" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-68052819-1', 'auto');
+    ga('send', 'pageview');
+
+  </script>
+
   <?php wp_head(); ?>
 </head>
 
