cf3e46ad3c5c85b91d0ed3f8086c41a8c34f008e
Patrick Cameron <patcameron@gmail.com>
2016-07-11 20:54:28 -0400

    Added google tag manager code

--- header.php
+++ header.php
@@ -11,7 +11,6 @@ ARROW ANIMATION: Justin Aguilar http://www.justinaguilar.com/animations/ -->
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="keywords" content="Patrick Cameron, Toronto front end developer, Toronto web designer, front end developer">
   <meta name="description" content="Front end web developer and designer based in Toronto.">
@@ -20,36 +19,27 @@ ARROW ANIMATION: Justin Aguilar http://www.justinaguilar.com/animations/ -->
   <meta property="og:url" content="http://www.patrickcameron.ca">
   <meta property="og:site_name" content="Patrick Cameron - front end developer, Toronto">
   <meta property="og:description" content="Front end developer and designer based in Toronto">
-
   <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
-
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-
   <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
   <link href='https://fonts.googleapis.com/css?family=Lato:400,300,100,700' rel='stylesheet' type='text/css'>
-
   <title>Patrick Cameron - front end developer - Toronto</title>
-
   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-  <?php // Load our CSS ?>
   <link rel="stylesheet" type="text/css" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
-
-  <script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-    ga('create', 'UA-68052819-1', 'auto');
-    ga('send', 'pageview');
-
-  </script>
-
   <?php wp_head(); ?>
 </head>
 
-
 <body <?php body_class(); ?>>
+
+<!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-NWH68Q"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NWH68Q');</script>
+<!-- End Google Tag Manager -->
 
 <nav class="clearfix">
   <ul class="socialIcons">
